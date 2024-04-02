678c2c58fe3405d12c8c4aef4a582a68ff5b09d8
Laboratorio Unidade Fatima <labunivas@univas.edu.br>
2024-04-01 21:45:56 -0300

    Eduardo-Borges

---
+++ src/br/edu/univas/stringmanipulator/StringManipulator.java
@@ -0,0 +1,45 @@
+package br.edu.univas.stringmanipulator;
+
+public class StringManipulator {
+
+    public int getNumeroVogais(String str) {
+        int count = 0;
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getNumeroConsoantes(String str) {
+        int count = 0;
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if ((ch >= 'a' && ch <= 'z') && !(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean getIsAnagrama(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < str1.length(); i++) {
+            count[str1.charAt(i) - 'a']++;
+            count[str2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
